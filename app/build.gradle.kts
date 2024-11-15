b6e2fbbf22fdf38dbbe4ff44f79751bc99d320d4
sudeis <sudeisfed@gmail.com>
2024-11-15 17:49:47 +0300

    updating gradle setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.class_attendace_app"
-    compileSdk = 34
+    compileSdk = 35 // Or change this to 34 if you prefer
 
     defaultConfig {
         applicationId = "com.example.class_attendace_app"
@@ -29,19 +29,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,7 +55,7 @@ android {
 }
 
 dependencies {
-
+    // AndroidX libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,11 +64,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
+
+    // Testing libraries
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debugging libraries
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/example/class_attendace_app/LoginActivity.kt
+++ app/src/main/java/com/example/class_attendace_app/LoginActivity.kt
@@ -1,4 +1,27 @@
 package com.example.class_attendace_app
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+        val loginButton = findViewById<Button>(R.id.log_btn)
+
+        loginButton.setOnClickListener{
+            val  intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
+
+
+
+
+
